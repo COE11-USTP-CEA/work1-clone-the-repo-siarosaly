6474bd2df1f05ec10a21908a77f1166e20067ff5
Rosaly Sia <RosalySia@users.noreply.github.com>
2016-12-09 02:10:11 -0800

    Update student.java

--- student.java
+++ student.java
@@ -2,32 +2,32 @@ import java.util.List;
 import java.util.ArrayList;
 
 class student{
-	String stud_name;
-	String stud_dept;
+				String stud_name;
+				String stud_dept;
+				
+				List<subject> subjects = new ArrayList<subject>();
 
-	List<subject> subjects = new ArrayList<subject>();	
-
-	public void addSubject(subject s){
-		subjects.add(s);
-	}
-
-	public List<subject> getSubjects(){
-		return subjects;
-	}
-
-	public void setName(String name){
-		stud_name = name;	
-	}	
-
-	public String getName(){
-		return stud_name;
-	}	
-
-	public void setDept(String dept){
-		stud_dept = dept;
-	}
-
-	public String getDept(){
-		return stud_dept;
-	}
+				public void addSubject(subject s){
+						subjects.add(s);
+				}
+	
+				public List<subject> getSubjects(){
+								return subjects;
+				}
+	
+				public void setName(String name){
+						stud_name = name;	
+				}
+	
+				public String getName(){
+								return stud_name;
+				}
+	
+				public void setDept(String dept){
+								stud_dept = dept;
+				}
+	
+				public String getDept(){
+								return stud_dept;
+				}
 }
